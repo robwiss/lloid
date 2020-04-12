f3128b214a6eb632b383848f9e39889913d6a729
Wessel Kuipers <wesselkuipers@gmail.com>
2020-04-12 19:18:35 +0200

    implement logging

--- lloidbot.py
+++ lloidbot.py
@@ -8,11 +8,13 @@ from dotenv import load_dotenv
 import os
 import re
 import sentry_sdk
+import logging
+import argparse
 
 queue = []
 queue_interval = 60*10
 poll_sleep_interval = 5
-
+logger = logging.getLogger('lloid')
 
 class Command:
     Successful = 0
@@ -26,7 +28,7 @@ class Command:
     Next = 6
 
     def __init__(self, command):
-        print(command.strip().lower())
+        logger.debug(command.strip().lower())
         if command.strip().lower() == "close":
             self.status = Command.Successful
             self.cmd = Command.Close
@@ -89,9 +91,9 @@ class Lloid(discord.Client):
     QueueEmpty = 2
 
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        logger.info('Logged on as {0}!'.format(self.user))
         if self.initialized is None or not self.initialized:
-            print("Initializing.")
+            logger.info("Initializing.")
             self.initialized = True
             self.report_channel = self.get_channel(int(os.getenv("ANNOUNCE_ID")))
             self.chan = 'global'
@@ -107,8 +109,8 @@ class Lloid(discord.Client):
 
             deleted = await self.report_channel.purge(check=lambda m: m.author==client.user)
             num_del = len(deleted)
-            print(f"Initialized. Deleted {num_del} old messages.")
-        print(f"Sample data to verify data integrity: {self.associated_user}")
+            logger.info(f"Initialized. Deleted {num_del} old messages.")
+        logger.info(f"Sample data to verify data integrity: {self.associated_user}")
 
     async def on_raw_reaction_add(self, payload):
         channel = await client.fetch_channel(payload.channel_id)
@@ -118,7 +120,7 @@ class Lloid(discord.Client):
         if user == client.user or message.author != client.user:
             return
         if payload.emoji.name == '🦝':
-            print(f"{user.name} reacted with raccoon")
+            logger.debug(f"{user.name} reacted with raccoon")
             await self.queue_user(payload.message_id, user)
 
     async def queue_user(self, message_id, user):
@@ -126,13 +128,14 @@ class Lloid(discord.Client):
             status, size = self.market.request(user.id, self.associated_user[message_id])
             if status:
                 owner_name = self.get_user(self.associated_user[message_id]).name
-                print(f"queued {user.name} up for {owner_name}")
+                logger.info(f"queued {user.name} up for {owner_name}")
 
                 if size == 0:
                     size = 1
                 interval_s = queue_interval * (size - 1) // 60
                 interval_e = queue_interval * size // 60
-                await user.send(f"Queued you up for a dodo code for {owner_name}. Estimated time: {interval_s}-{interval_e} minutes, give or take (it waits 10 minutes for each person before letting someone in, but the people ahead of you may finish early and let you in earlier). "
+                await user.send(f"Queued you up for a dodo code for {owner_name}. Estimated time: {interval_s}-{interval_e} minutes, give or take "
+                "(it waits 10 minutes for each person before letting someone in, but the people ahead of you may finish early and let you in earlier). "
                 "If you want to queue up elsewhere, or if you have to go, just unreact and it'll free you up.\n\n"
                 "In the meantime, please be aware of common courtesy--once you have the code, it's possible for you to come back in any time you want. "
                 "However, please don't just do so willy-nilly, and instead, **requeue and use the bot as a flow control mechanism, even if you already know the code**. "
@@ -143,19 +146,19 @@ class Lloid(discord.Client):
                 await user.send("It sounds like either the market is now closed, or you're in line elsewhere at the moment.")
         else:
             k = self.associated_user.keys
-            print(f"{message_id} was not found in {k}")
+            logger.info(f"{message_id} was not found in {k}")
 
     async def on_raw_reaction_remove(self, payload):
         if payload.emoji.name == '🦝' and payload.message_id in self.associated_user and payload.user_id in self.market.queue.requesters:
             user = await client.fetch_user(payload.user_id)
-            print(f"{user.name} unreacted with raccoon")
+            logger.debug(f"{user.name} unreacted with raccoon")
             owner_name = self.get_user(self.associated_user[payload.message_id]).name
             waiting_for = self.market.queue.requesters[payload.user_id]
             if waiting_for == self.associated_user[payload.message_id] and self.market.forfeit(payload.user_id):
                 await user.send("Removed you from the queue for %s." % owner_name)
 
     async def on_disconnect(self):
-        print("Lloid got disconnected.")
+        logger.warning("Lloid got disconnected.")
 
     async def let_next_person_in(self, owner):
         task = None
@@ -163,44 +166,45 @@ class Lloid(discord.Client):
         if status == turnips.Status.QUEUE_EMPTY:
             return Lloid.QueueEmpty
         elif status == turnips.Status.ALREADY_CLOSED: # Then the owner closed
-            print(f"Closed queue for {owner}")
+            logger.info(f"Closed queue for {owner}")
             return Lloid.AlreadyClosed
 
-        print(f"Letting {self.get_user(task[0]).name} in to {task[1].name}")
+        logger.info(f"Letting {self.get_user(task[0]).name} in to {task[1].name}")
         await self.get_user(task[0]).send(f"Hope you enjoy your trip to **{task[1].name}**'s island! "
-        "Be polite, observe social distancing, leave a tip if you can, and **please be responsible and message me \"__done__\" when you've left (unless the island already has a lot of visitors inside, in which case... don't bother)**. Doing this lets the next visitor in."
+        "Be polite, observe social distancing, leave a tip if you can, and **please be responsible and message me \"__done__\" when you've left "
+        "(unless the island already has a lot of visitors inside, in which case... don't bother)**. Doing this lets the next visitor in."
         f"The Dodo code is **{task[1].dodo}**.")
         q = self.market.queue.queues[owner]
-        print(f"Remainder in queue = {len(q)}")
+        logger.info(f"Remainder in queue = {len(q)}")
         if len(q) > 0:
-            print(f"looking up {q[0][0]}")
+            logger.info(f"looking up {q[0][0]}")
             next_in_line = self.get_user(q[0][0])
             if next_in_line is not None:
-                print("Sending warning")
+                logger.info("Sending warning")
                 await next_in_line.send((f"Your flight to **{task[1].name}**'s island is boarding soon! "
                 "Please have your tickets ready, we'll be calling you in shortly!"))
-        print(f"{self.get_user(task[0]).name} has departed for {task[1].name}'s island")
+        logger.info(f"{self.get_user(task[0]).name} has departed for {task[1].name}'s island")
         self.recently_departed[task[0]] = owner
         try:
             await self.associated_message[owner].remove_reaction('🦝', self.get_user(task[0]))
         except Exception as ex:
-            print("Couldn't remove reaction; error: %s" % ex)
+            logger.warning("Couldn't remove reaction; error: %s" % ex)
 
-        print("should have been successful")
+        logger.debug("should have been successful")
         return Lloid.Successful
 
     async def reset_sleep(self, owner):
-        print("resetting sleep")
+        logger.info("Resetting sleep")
         if owner in self.sleepers:
-            print("cancelling current sleep")
+            logger.info("Cancelling current sleep")
             self.sleepers[owner].cancel()
         self.sleepers[owner] = self.loop.create_task(asyncio.sleep(queue_interval))
 
         try:
             await self.sleepers[owner]
-            print("Timeout on last visitor, letting next person in.")
+            logger.info("Timeout on last visitor, letting next person in.")
         except:
-            print("sleep was cancelled")
+            logger.info("Sleep was cancelled")
             pass
 
         if owner in self.sleepers:  # not yet sure why sometimes owner is not in self.sleepers
@@ -214,7 +218,7 @@ class Lloid(discord.Client):
             # we can't move that reset_sleep call up here because that means it would sleep before handing
             # out the first code.
             while owner in self.requested_pauses and self.requested_pauses[owner] > 0:
-                print(f"Sleeping upon request, {self.requested_pauses[owner]}")
+                logger.info(f"Sleeping upon request, {self.requested_pauses[owner]}")
                 self.is_paused[owner] = True
                 self.requested_pauses[owner] -= 1
                 await self.reset_sleep(owner)
@@ -226,12 +230,12 @@ class Lloid(discord.Client):
                 await asyncio.sleep(poll_sleep_interval)
                 continue
             elif status == Lloid.AlreadyClosed:
-                print("Lloid apparently closed")
+                logger.warning("Lloid apparently closed")
                 break
 
             print("Should reset sleep now")
             await self.reset_sleep(owner)
-        print("Exited the loop. This can only happen if the queue was closed.")
+        logger.warning("Exited the loop. This can only happen if the queue was closed.")
 
     async def handle_queueinfo(self, message):
         guest = message.author.id
@@ -259,7 +263,7 @@ class Lloid(discord.Client):
             return
 
         if isinstance(message.channel, discord.DMChannel):
-            print(f">>>> (PM) {message.author.name}: {message.content}")
+            logger.info(f">>>> (PM) {message.author.name}: {message.content}")
             command = Command(message.content)
 
             if command.status == Command.Successful:
@@ -311,13 +315,13 @@ class Lloid(discord.Client):
                         "The queue is actually paused at the moment, so the host will be the one to let the next person in.")
                         return
                     if owner is not None and owner in self.sleepers:
-                        print("Visitor done, cancelling timer")
+                        logger.info("Visitor done, cancelling timer")
                         self.sleepers[owner].cancel()
-                        print("Timer cancelled, thanking visitor")
+                        logger.info("Timer cancelled, thanking visitor")
                         await message.channel.send("Thanks for the heads-up! Letting the next person in now.")
                     elif owner is not None:
                         owner_name = self.get_user(owner).name
-                        print(f"Visitor marked themselves as done, but owner {owner_name} was not in sleepers")
+                        logger.info(f"Visitor marked themselves as done, but owner {owner_name} was not in sleepers")
                         await message.channel.send("Thanks for the heads-up! Letting the next person in now.")
                 else:
                     res = self.market.declare(message.author.id, message.author.name, command.price, command.dodo, command.tz)
@@ -325,7 +329,7 @@ class Lloid(discord.Client):
                         await message.channel.send("Updated your info. Anyone still in line will get the updated codes.")
                     elif res == turnips.Status.SUCCESS:
                         if message.author.id in self.sleepers:
-                            print("Owner has previous outstanding timers. Cancelling them now.")
+                            logger.info("Owner has previous outstanding timers. Cancelling them now.")
                             self.sleepers[message.author.id].cancel()
                         self.requested_pauses[message.author.id] = 0
                         await message.channel.send("Okay! Please be responsible and message \"**close**\" to indicate when you've closed. "
@@ -349,16 +353,18 @@ class Lloid(discord.Client):
                         self.loop.create_task(self.queue_manager(message.author.id))
                     elif res == turnips.Status.TIMEZONE_REQUIRED:
                         await message.channel.send(("This seems to be your first time setting turnips, "
-                        "so you'll need to provide both a dodo code and a GMT offset (just a positive or negative integer). The dodo code can be a placeholder if you want."))
+                        "so you'll need to provide both a dodo code and a GMT offset (just a positive or negative integer). "
+                        "The dodo code can be a placeholder if you want."))
                     elif res == turnips.Status.PRICE_REQUIRED:
                         await message.channel.send("You'll need to tell us how much the turnips are at least.")
                     elif res == turnips.Status.DODO_REQUIRED:
                         await message.channel.send(("This seems to be your first time setting turnips, "
-                        "so you'll need to provide both a dodo code and a GMT offset (just a positive or negative integer). The dodo code can be a placeholder if you want."))
+                        "so you'll need to provide both a dodo code and a GMT offset (just a positive or negative integer). "
+                        "The dodo code can be a placeholder if you want."))
                     elif res == turnips.Status.ITS_SUNDAY:
                         await message.channel.send("I'm afraid the turnip prices aren't set on Sundays, so will you please come again tomorrow instead?")
                     elif res == turnips.Status.CLOSED:
-                        print("This message should no longer be reachable (status = closed)")
+                        logger.info("This message should no longer be reachable (status = closed)")
             else:
                 await message.channel.send("Usage: \"[price] [optional dodo code] [optional gmt offset--an integer such as -5 or 8] [optional description, markdown supported]\"\n\n "
                 "The quotes (\")and square brackets ([]) are not part of the input!\n\n"
@@ -371,9 +377,25 @@ class Lloid(discord.Client):
         if message.content == "!queueinfo":
             await self.handle_queueinfo(message)
 
-
 if __name__ == "__main__":
-    print ("Starting Lloid...")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='count', help='Sets the verbosity level of the logger.', default=0, required=False)
+    args = parser.parse_args()
+    verbosity = args.verbose
+    log_level = logging.WARNING
+    
+    if verbosity >= 2:
+        log_level = logging.DEBUG
+    elif verbosity >= 1:
+        log_level = logging.INFO
+    elif verbosity <= 0:
+        log_level = logging.WARNING
+
+    logging.basicConfig(format='[%(asctime)s] %(levelname)s %(filename)s@%(lineno)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')  
+    logger.setLevel(log_level)
+    logger.info(f"Set logging level to {logging.getLevelName(log_level)}")
+
+    logger.info("Starting Lloid...")
     load_dotenv()
     token = os.getenv("TOKEN")
     interval = os.getenv("QUEUE_INTERVAL")
@@ -387,11 +409,11 @@ if __name__ == "__main__":
 
     if sentry_dsn:
         sentry_sdk.init(sentry_dsn)
-        print("Connected to Sentry")
+        logger.info("Connected to Sentry")
 
     if interval:
         queue_interval = int(interval)
-        print(f"Set interval to {interval}")
+        logger.info(f"Set interval to {interval}")
 
     client = Lloid()
     client.initialized = False
